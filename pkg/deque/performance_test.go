dbf2f00f8894fe7f1eb070a9f07707bb74719102
Maks Kolman <maks.kolman@bitstamp.net>
2022-01-13 15:19:12 +0100

    Add benchmark tests for Deque

---
+++ pkg/deque/performance_test.go
@@ -0,0 +1,75 @@
+package deque
+
+import "testing"
+
+func BenchmarkChan_FillDrain(b *testing.B) {
+	c := make(chan int, b.N)
+	for i := 0; i < b.N; i++ {
+		c <- i
+	}
+
+	for i := 0; i < b.N; i++ {
+		<- c
+	}
+}
+
+func BenchmarkDeque_FillDrain(b *testing.B) {
+	d := NewDequeWithCapacity(b.N)
+	for i := 0; i < b.N; i++ {
+		d.PushBack(i)
+	}
+
+	for i := 0; i < b.N; i++ {
+		d.PopFront()
+	}
+}
+
+func BenchmarkChan_Push(b *testing.B) {
+	c := make(chan int, b.N)
+	for i := 0; i < b.N; i++ {
+		c <- i
+	}
+}
+
+func BenchmarkDeque_Push(b *testing.B) {
+	d := NewDequeWithCapacity(b.N)
+	for i := 0; i < b.N; i++ {
+		d.PushBack(i)
+	}
+}
+
+func BenchmarkChan_Queue(b *testing.B) {
+	c := make(chan int, 1)
+	for i := 0; i < b.N; i++ {
+		c <- i
+		<- c
+	}
+}
+
+func BenchmarkDeque_Queue(b *testing.B) {
+	d := NewDequeWithCapacity(1)
+	for i := 0; i < b.N; i++ {
+		d.PushBack(i)
+		d.PopFront()
+	}
+}
+
+func BenchmarkChan_Queue2(b *testing.B) {
+	c := make(chan int, 2)
+	for i := 0; i < b.N/2; i++ {
+		c <- i
+		c <- i
+		<- c
+		<- c
+	}
+}
+
+func BenchmarkDeque_Queue2(b *testing.B) {
+	d := NewDequeWithCapacity(2)
+	for i := 0; i < b.N/2; i++ {
+		d.PushBack(i)
+		d.PushBack(i)
+		d.PopFront()
+		d.PopFront()
+	}
+}
